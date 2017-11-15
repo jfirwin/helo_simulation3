711092d877644aba4f19f1a4641ae72a84f476f3
Foster Irwin <jfosterirwin@Fosters-MacBook-Air.local>
2017-11-14 18:06:52 -0700

    got authentication working correctly login/logout

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require('express')
     , cors = require('cors')
     , massive = require('massive')
     , Auth0Strategy = require('passport-auth0')
-    , passport = require('passport');
+    , passport = require('passport')
+    , logout = require('express-passport-logout');
+
 
 require('dotenv').config();
 
@@ -22,7 +24,7 @@ massive(process.env.CONNECTION_STRING).then(db => {
   app.set('db', db);
 })
 
-app.use(express.static(__dirname + '/helo_simulation3_frontend/build/'));
+// app.use(express.static(__dirname + '/helo_simulation3_frontend/build/'));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,34 +37,44 @@ passport.use(new Auth0Strategy({
   },
   function(accessToken, refreshToken, extraParams, profile, done){
     const db = app.get('db');
-
-    db.get_user([profile.identities[0].user.id])
+    db.get_user([profile.identities[0].user_id])
     .then( user => {
       if(user[0]) {
-        return done(null, { auth_id: user[0].id })
+        done(null, { id: user[0].auth_id })
       } else {
-        db.create_user([profile.identities[0].user.id])
+        db.create_user([profile.identities[0].user_id])
           .then( newUser => {
-            return done(null, { auth_id: user[0].id })
+            done(null, { id: user[0].auth_id })
           })
       }
   });
 }));
 
 app.get('/auth', passport.authenticate('auth0'));
+
 app.get('/auth/callback', passport.authenticate('auth0', {
-  successRedirect: 'http://localhost:3001/profile',
-  failureRedirect: 'http://localhost:3001/'
-}));
+  failureRedirect: process.env.AUTH_FAILURE_URL
+}),
+function(req, res) {
+  if(!req.user) {
+    throw new Error('user null');
+  }
+  res.redirect(process.env.AUTH_SUCCESS_URL)
+});
+
+app.get('/auth/logout', (req, res) => {
+  req.logout();
+  res.redirect(process.env.AUTH_LOGOUT_URL)
+})
 
 passport.serializeUser(function(user, done){
   done(null, user);
 });
 
 passport.deserializeUser(function(user, done){
-  app.get('db').get_user([user.auth_id])
-    .then( user => {
-      return done(null, user[0]);
+  app.get('db').get_user([user.id])
+    .then( db_user => {
+      done(null, db_user[0].auth_id);
     });
 });
 
